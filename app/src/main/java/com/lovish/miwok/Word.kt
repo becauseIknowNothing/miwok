eaf68dfe323d89381fbe4043826cf2d4e01d0ba9
lovish <lovish.madhu.2404@gmail.com>
2020-05-22 03:26:16 +0530

    Updated

--- app/src/main/java/com/lovish/miwok/Word.kt
+++ app/src/main/java/com/lovish/miwok/Word.kt
@@ -1,13 +1,13 @@
 package com.lovish.miwok
+
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.list_item.view.*
+import androidx.core.content.ContextCompat
 
 
 class Word{
@@ -34,8 +34,8 @@ class WordAdapter(context: Context, words : ArrayList<Word>, backColor : Int) : 
         }
         val currposs = getItem(position) as Word
         val miwokTextView = listItemView!!.findViewById(R.id.miwok_text_view) as TextView
-        val defaultTextView = listItemView!!.findViewById(R.id.default_text_view) as TextView
-        val imgView = listItemView!!.findViewById(R.id.img) as ImageView
+        val defaultTextView = listItemView.findViewById(R.id.default_text_view) as TextView
+        val imgView = listItemView.findViewById(R.id.img) as ImageView
         miwokTextView.text = currposs.miwok
         defaultTextView.text = currposs.default
         if(currposs.img!=-1){
@@ -44,7 +44,8 @@ class WordAdapter(context: Context, words : ArrayList<Word>, backColor : Int) : 
         } else{
             imgView.visibility = 8
         }
-        listItemView.setBackgroundColor(backColor)
+        val color = ContextCompat.getColor(context, backColor)
+        listItemView.setBackgroundColor(color)
         return listItemView
     }
 }
